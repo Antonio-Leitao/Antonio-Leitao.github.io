a4a873254aa780eeed634d0dadba6e42953f0d32
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2022-08-26 11:35:11 +0200

    push trial2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Antonio-Leitao.github.io/",
   plugins: [svelte()]
 })
